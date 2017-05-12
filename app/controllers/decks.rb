97aad9af3de3da85f951e7250d87b22b08b7d1df
Jason <email": null>
2017-05-12 16:43:28 -0400

    added route for rounds of deck

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -6,6 +6,17 @@ get '/decks/new' do
   erb :'/decks/new'
 end
 
+post '/cards' do
+  deck = Deck.find_by
+  card = Card.find_by(answer: params[:answer])
+  if card
+    deck.cards = deck.cards - [card]
+    @response = "correct"
+  else
+    @reponse = "incorrect"
+  end
+end
+
 post '/decks' do
   @deck = Deck.new(title: params[:title])
   if @deck.save
@@ -17,10 +28,15 @@ post '/decks' do
 end
 
 get '/decks/:id' do
+  @deck = Deck.find_by(id: params[:id])
+  @round = @deck.round
+  @cards_array = @round.deck.cards
+  @filtered = card_filter(@round, @cards_array)
   erb :'/decks/show'
 end
 
 get '/decks/:id/edit' do
+  @deck = Deck.find_by(id: params[:id])
   erb :'/decks/edit'
 end
 
--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,17 @@ class Round < ApplicationRecord
 belongs_to :deck
 belongs_to :user
 has_many :guesses
+
+  def card_filter(round, array)
+    round.deck.cards.each do |card|
+      card.guesses.each do |guess|
+        if card.answer == guess.attempt &&
+          round.id == guess.round_id
+          array.delete(card)
+        end
+      end
+    end
+    array
+  end
+
 end
