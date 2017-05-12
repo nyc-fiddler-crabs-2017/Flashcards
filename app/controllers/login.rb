72881b936931ad3ab3eb220f44c7d0a2ded6db8b
Alexandra Hill <alexandrahillolds@gmail.com>
2017-05-12 12:07:38 -0400

    Update login.rb

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -8,18 +8,13 @@ post '/login' do
   @user = User.find_by(email: params[:email])
   if @user.authenticate(params[:password])
     session[:user_id] = @user.id
-    redirect "/login/#{@user.id}"
+    redirect "/users/#{@user.id}"
   else
     "nope"
   end
 end
 
-get '/login/:id' do
-  @user = User.find_by(id: params[:id])
-  erb :'/users/index'
-end
-
-get '/logout' do
+delete '/logout' do
   session.clear
   redirect '/'
 end
