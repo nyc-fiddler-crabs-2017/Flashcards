fd171d5c148e2433f5296704c8c50c94dbcd9bd1
Alexandra Hill & Thomas Feng <pair+alexnhill.realtomfeng@devbootcamp.com>
2017-05-12 12:32:18 -0400

    Begin to add seed data

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Deck.create(title: Nighthawk Flashcards)
+
+nighthawk_cards = [ {question: 'What is a nighthawks primary food source?' answer: 'insects' deck_id: 1},
+  {question: 'True or false?  Nighthawks are closely related to hawks.' answer: 'false' deck_id: 1}, {question: 'True or false?  Nighthawks make nests.' answer: 'false' deck_id: 1}]
+
+Card.create #loop through above hash
